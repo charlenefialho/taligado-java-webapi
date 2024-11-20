6cfceab48fa8b6a56a07856c24412ac7d477de89
charlenefialho <fialhocharlene@gmail.com>
2024-11-20 14:29:16 -0300

    initial commit

---
+++ src/main/java/com/fiap/taligado/TaligadoApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.taligado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaligadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaligadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/taligado/TaligadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.taligado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaligadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
