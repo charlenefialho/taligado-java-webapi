df0ed6c252e43b91bcc61195125dbb1cda5290d1
charlenefialho <fialhocharlene@gmail.com>
2024-11-22 01:11:05 -0300

    fix: style formchat assitant

--- src/main/java/com/fiap/taligado/controller/BotAssistantController.java
+++ src/main/java/com/fiap/taligado/controller/BotAssistantController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestClientException;
 
 @Controller
 public class BotAssistantController {
@@ -19,14 +20,22 @@ public class BotAssistantController {
 		return "form_bot_assistant";
 	}
 	
+	
 	@PostMapping("/enviar_chat")
-	public String enviarPerguntaChatGPT(@RequestParam(name = "pergunta") String pergunta, 
-			Model model) {
-		String resposta = chatClient.call(pergunta);
-		
-		String respostaFormatada = resposta.replace("\n", "<br>");
+	public String enviarPerguntaChatGPT(@RequestParam(name = "pergunta") String pergunta, Model model) {
+	    try {
+	        String resposta = chatClient.call(pergunta);
 
-	    model.addAttribute("resposta", respostaFormatada);
-	    return "resposta_bot";
+	        // Formatar resposta para HTML
+	        String respostaFormatada = resposta.replace("\n", "<br>");
+	        model.addAttribute("resposta", respostaFormatada);
+	        return "resposta_bot";
+
+	    } catch (RestClientException e) {
+	        e.printStackTrace();
+	        model.addAttribute("resposta", "Ocorreu um erro ao processar sua solicitação. Por favor, tente novamente.");
+	        return "resposta_bot";
+	    }
 	}
+
 }
